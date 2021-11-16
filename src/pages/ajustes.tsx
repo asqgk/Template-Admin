aea4fe8a5cd983c95472bd60c72f47a486111c1c
Francisco <chico.81@hotmail.com>
2021-11-16 15:13:01 -0300

    Initial Commit

---
+++ src/pages/ajustes.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/template/Layout";
+
+export default function Ajustes() {
+  return (
+      <Layout titulo="Ajustes"
+          subtitulo="Ajst">
+      <h3>Conteúdo ...</h3>
+    </Layout>
+  )
+}
---
+++ src/pages/notificacoes.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/template/Layout";
+
+export default function Notificacoes() {
+  return (
+      <Layout titulo="Notificações"
+          subtitulo="Notf">
+      <h3>Conteúdo ...</h3>
+    </Layout>
+  )
+}
